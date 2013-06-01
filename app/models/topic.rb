1e27b9a5d6e8b6b50e278202b14b950627de64e3
danners316 <daniel.reynolds7a@gmail.com>
2013-06-01 20:02:10 +0100

    first commit

---
+++ app/models/topic.rb
@@ -0,0 +1,14 @@
+class Topic < ActiveRecord::Base
+  attr_accessible :body, :name
+
+  belongs_to :user
+  has_many :posts, :dependent => :destroy
+
+  def self.search(search_query)
+    if search_query
+      find(:all,:conditions => ['body LIKE ?', "%#{search_query}%"])
+    else
+      find(:all)
+    end
+  end
+end
